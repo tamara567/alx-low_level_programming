08efe46d4b6d0d2deb2e2ebfeccb57dc552120c9
tamara567 <ringatunu@gmail.com>
2023-06-22 05:46:08 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-isupper.c -o 0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0 or 1
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+		return (0);
+	else
+		return (0);
+}
